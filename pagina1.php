e27fbdca2f2b04791aed66435e639f15c1f0d5ea
CarlosHdz7 <carloshdz.udb@gmail.com>
2018-03-19 19:20:53 -0600

    Firts commit

---
+++ pagina1.php
@@ -0,0 +1,19 @@
+<?php
+    session_start(); //inicio de sesion
+    $_SESSION['nombre'] = 'Carlos'; //este valor se puede capturar desde un textbox
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>sesion</title>
+    <h1>Pagina de inicio</h1>
+    <p></p>
+    <a href="pagina2.php">Ir a la pagina 2</a>
+</head>
+<body>
+    
+</body>
+</html>
